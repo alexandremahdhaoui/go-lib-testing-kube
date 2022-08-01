4b90dab763a32d8bd4b0c31153615fab9c63d0a8
alexandre <alexandre.mahdhaoui@share-now.com>
2022-08-02 00:19:52 +0200

    fix(await): updated await_resource

--- await_resource.go
+++ await_resource.go
@@ -2,16 +2,23 @@ package tKube
 
 import (
 	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/testing"
 	"time"
 )
 
-// AwaitResource
-//  - Waits until specified resource is available
+type resources map[string]func(testing.TestingT, *k8s.KubectlOptions, string, int, time.Duration)
+
+// AwaitResource sleeps until specified resource is available
 func AwaitResource(k KubeTester, resource string, retries int, sleep time.Duration) {
-	switch resource {
-	case "ingress":
-		k8s.WaitUntilIngressAvailable(k.T(), k.KubeOpt(), k.Id(), retries, sleep)
-	case "service":
-		k8s.WaitUntilServiceAvailable(k.T(), k.KubeOpt(), k.Id(), retries, sleep)
+	m := resources{
+		"ingress": k8s.WaitUntilIngressAvailable,
+		"service": k8s.WaitUntilServiceAvailable,
 	}
+	for K, v := range m {
+		if K == resource {
+			v(k.T(), k.KubeOpt(), k.Id(), retries, sleep)
+			return
+		}
+	}
+	k.T().Fatalf("specified resource is not implemented; got: %s", resource)
 }
